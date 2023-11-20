551884bdabca6b3ede46127c400c2fa8dc5ac4fb
zhoulq <zhoulq@citydo.com.cn>
2023-11-20 13:09:05 +0800

    release version v0.1.0

---
+++ calc.go
@@ -0,0 +1,21 @@
+package calc
+
+// Add 返回两个整数和的函数
+func Add(x, y int) int {
+	return x + y
+}
+
+// Add 返回两个整数差的函数
+func Subtract(x, y int) int {
+	return x - y
+}
+
+// Add 返回两个整数相乘的函数
+func Multiply(x, y int) int {
+	return x * y
+}
+
+// Add 返回两个整数相除的函数
+func Divide(x, y int) int {
+	return x * y
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zlq1503/calc
+
+go 1.21.3
